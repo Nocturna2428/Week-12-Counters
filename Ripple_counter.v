998bd1917eae14fd09227f35b3d64e31ea8c6450
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-12 20:28:28 -0700

    Update Ripple_counter.v

--- Ripple_counter.v
+++ Ripple_counter.v
@@ -4,7 +4,11 @@ module T_flip_flop(
   else
     Q <= ~Q;
     );
-    
+end
+TFF stage0(.clk(btnC), .reset(btnU), .Q(led[0]));
+TFF stage1(.clk(led[0]), .reset(btnU), .Q(led[1]));
+TFF stage2(.clk(led[1]), .reset(btnU), .Q(led[2]));
+     
      D_Flip_Flop dflip(
     .D(inp),
     .C(C),
