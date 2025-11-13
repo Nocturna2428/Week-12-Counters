8f1333859b39344fab117257dd0a7c9eb4036cb8
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-11-13 13:55:52 -0700

    Update and rename Counter.v to d_ff.v

--- Counter.v
+++
@@ -1,5 +0,0 @@
-Counter
-module counter(btnC,btnU);
-  input btnC,btnU;
-  output [6:0] data;
-
---
+++ d_ff.v
@@ -0,0 +1,14 @@
+module d_ff(
+    input clk,
+    input reset,
+    input D,
+    output reg Q
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            Q <= 0;
+        else
+            Q <= D;
+    end
+endmodule
+
